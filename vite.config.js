3ce0bff3c0aa8a8a9e8556552d80029f7d17e8f2
Nikaio <nicollas.laessandro@gmail.com>
2026-03-09 10:28:05 -0300

    Correção: adicionar vercelPreset à configuração do reactRouter

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'; // Importe o plugin
+import { vercelPreset } from '@vercel/react-router';
 
 export default defineConfig({
   plugins: [
-    reactRouter(),
-    tailwindcss(), 
+    reactRouter({
+      presets: [vercelPreset()],
+    }),
+    tailwindcss(),
   ],
 });
